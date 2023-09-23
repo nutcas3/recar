426174b0972aa4581b594046c0aa054ceb89b3c3
nutcas3 <mauricee423@gmail.com>
2023-09-23 16:31:07 +0300

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // src/App.js
-import React, { useState } from "react";
-import "./App.css";
-import LoanForm from "./components/LoanForm";
-import PredictionResult from "./components/PredictionResult";
+import React, { useState } from 'react';
+import './App.css';
+import LoanForm from './components/LoanForm';
+import PredictionResult from './components/PredictionResult';
 
 function App() {
-  const [predictionResult, setPredictionResult] = useState("");
+  const [predictionResult, setPredictionResult] = useState('');
 
   const handlePredict = (applicantData) => {
     // Extract data from the applicantData object
@@ -13,36 +13,36 @@ function App() {
       income,
       loanAmount,
       loanTerm,
-      assets,
-      mortgage,
+      assets, // Add assets field
+      mortgage, // Add mortgage field
     } = applicantData;
-  
+
     // Calculate wealthTotal as the sum of income and assets
     const wealthTotal = income + assets;
-  
+
     // Calculate liabilityTotal as the sum of loanAmount and mortgage
-    const liabilityTotal = loanAmount + mortgage;
-  
+    const liabilityTotal = (loanAmount *loanTerm)+ mortgage;
+
     // Calculate the wealth-to-liability ratio as a percentage
     const wealthToLiabilityRatio = (wealthTotal / liabilityTotal) * 100;
-  
+
     // Determine the credit score based on the ratio
-    let creditScore = "";
-  
-    if (wealthToLiabilityRatio < 20) {
-      creditScore = "Fair Credit Score";
-    } else if (wealthToLiabilityRatio >= 20 && wealthToLiabilityRatio <= 40) {
-      creditScore = "Medium Credit Score";
+    let creditScore = '';
+
+    if (wealthToLiabilityRatio <= 20) {
+      creditScore = 'Poor Credit Score';
+    } else if (wealthToLiabilityRatio <= 40) {
+      creditScore = 'Fair Credit Score';
     } else {
-      creditScore = "High Risk Credit Score";
+      creditScore = 'High Risk';
     }
-  
+
     setPredictionResult(creditScore);
   };
-  
-  
+
   return (
     <div className="App">
+
       <LoanForm onPredict={handlePredict} />
       {predictionResult && <PredictionResult result={predictionResult} />}
     </div>
